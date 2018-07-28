77feea641f4e879ba58b61c17b3bf1f1440442d6
Jigar <jigaragrawal29@gmail.com>
2018-07-28 13:54:39 -0700

    selection Sort Completed

--- src/main.c
+++ src/main.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "selection_sort.h"
+
+void printArray(const int *array, int size)
+{
+	if(size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	printf("%d, ", *array);
+	printArray(array+1, size-1);
+}
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+	const int data[6] = {5, 8, 3, 2, 1, 10};
+	int *sorted_array = 0;
+	printf("Unsorted Array \n");
+	printArray(&data[0], 6);
+
+	/* Selection Sort */
+	printf("Performing the Selection Sort:- \n");
+	printf("Output From Selection Sort:- \n");
+	sorted_array = selectionSort(&data[0], 6);
+	printArray(sorted_array, 6);
+	free(sorted_array);
+
+	/* Bubble Sort */
+	printf("Performing the Bubble Sort:- \n");
+	printf("Output From Bubble Sort:- \n");
+	sorted_array = bubbleSort(&data[0], 6);
+	printArray(sorted_array, 6);
+	free(sorted_array);
+
+	return 0;
 }
--- src/selection_sort/selection_sort.c
+++ src/selection_sort/selection_sort.c
@@ -5,4 +5,26 @@
  *      Author: Jigar
  */
 
+#include "selection_sort.h"
+#include "stdlib.h"
+#include <string.h>
 
+int* selectionSort(const int *data, int size)
+{
+	int temp = 0;
+	int *array= (int *)calloc(size, sizeof(int));
+	memcpy(array, data, size * sizeof(int));
+	for(int i=0; i<(size-1); i++)
+	{
+		for(int j=i; j<(size-1);j++)
+		{
+			if(array[i] > array[j])
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+			}
+		}
+	}
+	return array;
+}
